b22c8cf368d01bffe5a6deedc4664c91e0e02be3
Amrit-raj50 <amrit.raj.cg@gmail.com>
2026-05-25 22:51:12 +0530

    Total Request feature added for the tool

---
+++ log/src/components/TotalReq.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {useState} from 'react';
+
+export default function TotalReq({file}){
+    // const [req , setReq] = useState(0);
+
+    let req = 0;
+
+    const lines = file.split('\n'); // spliting the file on the basis of lines
+
+    lines.forEach(line => { // checking valid line and counting total valid request
+        if(line.trim() !== ''){
+            req++;
+        }
+    });
+    // setReq(req);//set the frequency of valid request
+
+    return(
+        <>
+        {/* {summary && ( */}
+                <div style={{ marginTop: '20px', padding: '10px', border: '1px solid black' }}>
+                    <h2>Report</h2>
+                    <p>total request processed : {req}</p>
+                </div>
+            {/* )} */}
+        </>
+    )
+}
